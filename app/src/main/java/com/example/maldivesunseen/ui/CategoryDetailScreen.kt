a928318cd138a42867ec3229c07510c297d259e3
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-07-07 07:38:49 +1000

    Add destination UI screens

---
+++ app/src/main/java/com/example/maldivesunseen/ui/CategoryDetailScreen.kt
@@ -0,0 +1,9 @@
+package com.example.maldivesunseen.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CategoryDetailScreen(modifier: Modifier = Modifier) {
+
+}
