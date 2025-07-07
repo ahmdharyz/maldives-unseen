9af859693af5d4ce5265a9c4f7932296a1531c93
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-07-08 06:14:44 +1000

    Import StringRes

--- app/src/main/java/com/example/maldivesunseen/ui/MaldivesUnseenApp.kt
+++ app/src/main/java/com/example/maldivesunseen/ui/MaldivesUnseenApp.kt
@@ -1,5 +1,6 @@
 package com.example.maldivesunseen.ui
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
