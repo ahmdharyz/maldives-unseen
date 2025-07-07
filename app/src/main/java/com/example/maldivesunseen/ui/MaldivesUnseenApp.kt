16625a3dd01342b2a303baf8ffd0f14f4ec4a23b
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-07-08 06:09:16 +1000

    Add Scaffold for shared UI structure

--- app/src/main/java/com/example/maldivesunseen/ui/MaldivesUnseenApp.kt
+++ app/src/main/java/com/example/maldivesunseen/ui/MaldivesUnseenApp.kt
@@ -1,8 +1,14 @@
 package com.example.maldivesunseen.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun MaldivesUnseenApp(modifier: Modifier = Modifier) {
+    Scaffold {
+        paddingValues ->
+        modifier.padding(paddingValues)
+    }
 }
