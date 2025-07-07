5c70d0d7f64fa42dc6c429caddbeb321315ddd3b
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-07-07 21:21:49 +1000

    Add enum class to define the routes

--- app/src/main/java/com/example/maldivesunseen/ui/MaldivesUnseenApp.kt
+++ app/src/main/java/com/example/maldivesunseen/ui/MaldivesUnseenApp.kt
@@ -4,6 +4,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.maldivesunseen.R
+
+// Is it necessary to create string values for the dynamic screens such as CategoryDetail and Recommendation?
+enum class MaldivesUnseenApp(@StringRes val title: Int) {
+    Home(title = R.string.app_name),
+    Categories(title = R.string.category),
+    CategoryDetail(title = R.string.category_detail),
+    Recommendation(title = R.string.recommendation),
+}
 
 @Composable
 fun MaldivesUnseenApp(modifier: Modifier = Modifier) {
