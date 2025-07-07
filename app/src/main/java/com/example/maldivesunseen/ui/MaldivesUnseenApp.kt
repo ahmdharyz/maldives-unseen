5250089babeb0ba82535bf1a1aeda2664e0f499c
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-07-08 07:44:46 +1000

    Add composable routes in NavHost

--- app/src/main/java/com/example/maldivesunseen/ui/MaldivesUnseenApp.kt
+++ app/src/main/java/com/example/maldivesunseen/ui/MaldivesUnseenApp.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.example.maldivesunseen.R
 
 // Is it necessary to create string values for the dynamic screens such as CategoryDetail and Recommendation?
@@ -26,6 +27,23 @@ fun MaldivesUnseenApp(modifier: Modifier = Modifier) {
             navController = navController,
             startDestination = MaldivesUnseenApp.Home.name,
             modifier = modifier
-        )
+        ) {
+            composable(route = MaldivesUnseenApp.Home.name) {
+                HomeScreen()
+            }
+            
+            composable(route = MaldivesUnseenApp.Categories.name) {
+                CategoriesScreen()
+
+            }
+
+            composable(route = MaldivesUnseenApp.CategoryDetail.name) {
+                CategoryDetailScreen()
+            }
+
+            composable(route = MaldivesUnseenApp.Recommendation.name) {
+                RecommendationScreen()
+            }
+        }
     }
 }
