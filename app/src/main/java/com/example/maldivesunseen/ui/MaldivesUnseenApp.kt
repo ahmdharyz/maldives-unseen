8af60a19db624970977a25b00ce83e4ce0cd4828
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-07-10 10:34:36 +1000

    Refactor navController creation in AppNavBar

--- app/src/main/java/com/example/maldivesunseen/ui/MaldivesUnseenApp.kt
+++ app/src/main/java/com/example/maldivesunseen/ui/MaldivesUnseenApp.kt
@@ -22,10 +22,11 @@ enum class Destination(@StringRes val title: Int) {
 
 @Composable
 fun AppNavBar(
+    navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
     NavHost(
-        navController = rememberNavController(),
+        navController = navController,
         startDestination = Destination.Home,
         modifier = modifier
     ) {
