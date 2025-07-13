e6781fc0cc71c5b94574891a2d3f867d5ff5f208
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-07-14 06:02:57 +1000

    Refactor Destination class type from enum to sealed class

--- app/src/main/java/com/example/maldivesunseen/ui/MaldivesUnseenApp.kt
+++ app/src/main/java/com/example/maldivesunseen/ui/MaldivesUnseenApp.kt
@@ -1,22 +1,61 @@
 package com.example.maldivesunseen.ui
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.maldivesunseen.R
 
-// Is it necessary to create string values for the dynamic screens such as CategoryDetail and Recommendation?
-enum class Destination(@StringRes val title: Int) {
-    Home(title = R.string.app_name),
-    Categories(title = R.string.category),
-    CategoryDetail(title = R.string.category_detail),
-    Recommendation(title = R.string.recommendation),
+// Navigation destinations using sealed classes
+sealed class Destination(
+    val route: String,
+    val label: Int? = null,
+    val icon: ImageVector? = null,
+    val contentDescription: Int? = null
+) {
+    // Bottom navigation destinations
+    data object Home: Destination(
+        route = "Home",
+        label = R.string.home,
+        icon = Icons.Default.Home,
+        contentDescription = R.string.home_description
+    )
+
+    data object Categories: Destination(
+        route = "Categories",
+        label = R.string.category,
+        icon = Icons.AutoMirrored.Default.List, // TODO: Check if it's a bulleted list, if not add butteted list icon
+        contentDescription = R.string.category_description
+    )
+
+    data object Settings: Destination(
+        route = "Settings",
+        label = R.string.settings,
+        icon = Icons.Default.Settings,
+        contentDescription = R.string.settings_description
+    )
+
+    // Non-Navigation Bar destinations
+    data object CategoryDetail: Destination(
+        route = "Category/{categoryId}",
+    ) {
+        fun createRoute(categoryId: Int) = "Category/$categoryId"
+    }
+
+    data object Recommendation: Destination(
+        route = "Recommendation/{recommendationId}",
+    ) {
+        fun createRoute(recommendationId: Int) = "Recommendation/$recommendationId"
+    }
 }
 
 @Composable
@@ -29,16 +68,16 @@ fun AppNavHost(
         startDestination = Destination.Home,
         modifier = modifier
     ) {
-            composable(Destination.Home.name) {
+            composable(Destination.Home.route) {
                 HomeScreen()
             }
-            composable(Destination.Categories.name) {
+            composable(Destination.Categories.route) {
                 CategoriesScreen()
             }
-            composable(Destination.CategoryDetail.name) {
+            composable(Destination.CategoryDetail.route) {
                 CategoryDetailScreen()
             }
-            composable(Destination.Recommendation.name) {
+            composable(Destination.Recommendation.route) {
                 RecommendationScreen()
             }
     }
