92c2b8509a37778c2da336ee2b27d2d2994122a3
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-07-14 22:46:09 +1000

    Add BottomNavigationBar composable

--- app/src/main/java/com/example/maldivesunseen/ui/MaldivesUnseenApp.kt
+++ app/src/main/java/com/example/maldivesunseen/ui/MaldivesUnseenApp.kt
@@ -5,8 +5,17 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavHostController
@@ -55,6 +64,36 @@ sealed class Destination(
         route = "Recommendation/{recommendationId}",
     ) {
         fun createRoute(recommendationId: Int) = "Recommendation/$recommendationId"
+    }
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    val destinations = listOf(
+        Destination.Home,
+        Destination.Categories,
+        Destination.Settings
+    )
+
+    val startDestination = Destination.Home
+    var selectedDestination by rememberSaveable { mutableStateOf(startDestination.route) }
+
+    NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) { // TODO: Check what happens when you remove windowInsets
+        destinations.forEach { destination ->
+            NavigationBarItem(
+                selected = selectedDestination == destination.route,
+                onClick = {
+                    navController.navigate(destination.route)
+                    selectedDestination = destination.route
+                },
+                icon = {
+                    Icon(
+                        imageVector = destination.icon ?: Icons.Default.Home,
+                        contentDescription =  destination.contentDescription.toString()
+                    )
+                }
+            )
+        }
     }
 }
 
