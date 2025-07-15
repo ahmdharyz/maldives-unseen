fe194641076e1928d1ccaedae4341a10b5c48955
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-07-15 23:43:09 +1000

    Refactor composable routes to use routes instead of strings

--- app/src/main/java/com/example/maldivesunseen/ui/MaldivesUnseenApp.kt
+++ app/src/main/java/com/example/maldivesunseen/ui/MaldivesUnseenApp.kt
@@ -1,25 +1,15 @@
 package com.example.maldivesunseen.ui
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarDefaults
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -66,39 +56,6 @@ sealed class Destination(
         route = "Recommendation/{recommendationId}",
     ) {
         fun createRoute(recommendationId: Int) = "Recommendation/$recommendationId"
-    }
-}
-
-@Composable
-fun BottomNavigationBar(navController: NavHostController) {
-    val destinations = listOf(
-        Destination.Home,
-        Destination.Categories,
-        Destination.Settings
-    )
-
-    val startDestination = Destination.Home
-    var selectedDestination by rememberSaveable { mutableStateOf(startDestination.route) }
-
-    NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) { // TODO: Check what happens when you remove windowInsets
-        destinations.forEach { destination ->
-            NavigationBarItem(
-                selected = selectedDestination == destination.route,
-                onClick = {
-                    navController.navigate(destination.route)
-                    selectedDestination = destination.route
-                },
-                icon = {
-                    Icon(
-                        imageVector = destination.icon ?: Icons.Default.Home,
-                        contentDescription =  stringResource(destination.contentDescription ?: R.string.home_description)
-                    )
-                },
-                label = {
-                    Text(stringResource(destination.label ?: R.string.home))
-                }
-            )
-        }
     }
 }
 
